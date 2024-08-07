8d2633bff5de6b92a32086e1a919c197e92b638d
Jaden Shepherd <ijaden.shepherd@gmail.com>
2024-08-07 17:13:17 +0100

    Create Model

---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property string $description
+ * @property Carbon|null $completed_at
+ */
+class Task extends Model
+{
+    protected $fillable = [
+        "description",
+        "completed_at",
+    ];
+
+    protected $casts = [
+        "completed_at" => "datetime",
+    ];
+}
